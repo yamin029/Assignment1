8b884af1a9afbb4b6d6813c00ffd684ec0a78d43
yamin029 <55071387+yamin029@users.noreply.github.com>
2019-10-30 00:30:27 +0600

    bug fixed

--- js/jquery.js
+++ js/jquery.js
@@ -4,23 +4,41 @@ $('.btn-add').click(function(){
 $('.btn-cart').click(function(){
     window.open("cart.html","_self")
 })
-cart_items = JSON.parse(window.localStorage.getItem('cart_items'))
-var cart_item_length = cart_items.length
-$('.btn-cart').children("span").children("sup").text(cart_item_length)
-var products =[]
 
-// var products = [
-//     {
-//     title:"Mother Board",
-//     img:"images/motherboard.jpg",
-//     price:"200$",
-//     description:"Intel Z390 GAMING Motherboard with 10+2 Digital PWM Design, 2-Way CrossFire™ Multi-Graphics, USB 3.1 Gen2 Type-A, M.2 Thermal Guard, Intel GbE LAN with cFosSpeed, Smart Fan 5, Dual M.2, Dual Armor with Ultra Durable™ Technology, DualBIOS, CEC 2019"
-//     }
-//  ]
+cart_items = JSON.parse(window.localStorage.getItem('cart_items'))
+var cart_item_length
+if(cart_items != null){
+    cart_item_length = cart_items.length
+}
+else{
+    cart_item_length = 0
+}
+$('.btn-cart').children("span").children("sup").text(cart_item_length)
+//var products =[]
+
+//  var products = [
+//      {
+//      title:"Mother Board",
+//      img:"images/motherboard.jpg",
+//      price:"200$",
+//      description:"Intel Z390 GAMING Motherboard with 10+2 Digital PWM Design, 2-Way CrossFire™ Multi-Graphics, USB 3.1 Gen2 Type-A, M.2 Thermal Guard, Intel GbE LAN with cFosSpeed, Smart Fan 5, Dual M.2, Dual Armor with Ultra Durable™ Technology, DualBIOS, CEC 2019"
+//      }
+//   ]
  //console.log(products)
  //window.localStorage.setItem('products',JSON.stringify(products))
  var products = JSON.parse(window.localStorage.getItem('products'))
  console.log(products)
+ if(products == null)
+ {
+    var products = [
+        {
+        title:"Mother Board",
+        img:"images/motherboard.jpg",
+        price:"200",
+        description:"Intel Z390 GAMING Motherboard with 10+2 Digital PWM Design, 2-Way CrossFire™ Multi-Graphics, USB 3.1 Gen2 Type-A, M.2 Thermal Guard, Intel GbE LAN with cFosSpeed, Smart Fan 5, Dual M.2, Dual Armor with Ultra Durable™ Technology, DualBIOS, CEC 2019"
+        }
+     ]
+ }
  
  $.each(products,function(i,val){
      var insertproduct = $('<div class="card shadow-lg border-success m-3" style="width: 18rem;"><img src="" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title"></h5><p class="card-text"></p><div class="container-fluid row "><div class="container col-md-6"><p></p></div><a class="btn btn-outline-primary col-md-6 d-blo btn-add-to-card">Add to Cart</a></div></div></div>')
@@ -49,13 +67,21 @@ var products =[]
      console.log(products)
      
      window.localStorage.setItem('products',JSON.stringify(products))
+     window.open('index.html',"_self")
  })
  var cart_items = []
  $('.btn-add-to-card').click(function(){
-    var cart_item = $(this).parent().parent().children("h5").text()
+    var cart_item = []
+    cart_item = $(this).parent().parent().children("h5").text()
     
     cart_items = JSON.parse(window.localStorage.getItem('cart_items'))
-    cart_items.push(cart_item)
+    if(cart_items == null){
+        cart_items = [cart_item]
+    }
+    else{
+        cart_items.push(cart_item)
+    }
+    
     window.localStorage.setItem('cart_items',JSON.stringify(cart_items))
     location.reload(true)
  })
